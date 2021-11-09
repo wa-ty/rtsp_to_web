ce4f04674e5e0c751c9645b75ab755ab6e1eca79
wa-ty <connorbwaity@gmail.com>
2021-11-08 19:27:21 -0600

    doesn't run forever

--- stream.py
+++ stream.py
@@ -1,9 +1,11 @@
 import cv2, queue, threading
+from datetime import datetime, timedelta
 
 class VideoCapture:
   def __init__(self, name):
     self.cap = cv2.VideoCapture(name)
     self.q = queue.Queue()
+    self.last_interaction = datetime.now()
     t = threading.Thread(target=self.worker)
     t.daemon = True
     t.start()
@@ -21,12 +23,17 @@ class VideoCapture:
       self.q.put(frame)
 
   def read(self):
+    self.last_interaction = datetime.now()
     return self.q.get()
 
 
 def gen_frames(link):
   cap = VideoCapture(link)
   while True:
+    print('running')
+    if timedelta(seconds=5) < (datetime.now() - cap.last_interaction):
+      print('killing')
+      break
     frame = cap.read()
     ret, buffer = cv2.imencode('.jpg', frame)
     frame = buffer.tobytes()
